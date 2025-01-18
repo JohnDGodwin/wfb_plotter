9fad5afe2dae6bdd57df3260ae646242e97ed7de
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-18 08:31:11 -0600

    Delete extcmd_userscript2.sh

--- extcmd_userscript2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo systemctl $1 wfb-cluster-manager@gs.service
-sleep 3
-sudo systemctl status wfb-cluster-manager@gs.service
-
-# Always keep a small sleep before exit
-sleep 0.5
-exit 0
