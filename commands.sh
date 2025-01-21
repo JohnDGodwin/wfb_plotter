c8d2bc2f2d4f9bb0c9c163f3421fe1574b7f521c
JohhnGoblin <35317840+JohnDGodwin@users.noreply.github.com>
2025-01-21 03:09:28 -0600

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -27,8 +27,6 @@ sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 "sed -i \"/vid
 echo "setting camera resolution to $BITRATE"
 }
 
-#!/bin/bash
-
 # Function to update channel
 update_channel() {
     sshpass -p '12345' ssh -o StrictHostKeyChecking=no root@10.5.0.10 "sed -i '/^channel=/ s/=.*/=$CHANNEL/' /etc/wfb.conf"
